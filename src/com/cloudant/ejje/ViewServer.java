efb89b47aa33687150a1ebc0c93b313901ac082b
hardtke <david@hardtke.net>
2010-08-19 15:11:26 -0700

    delete everything not associated with JavaView

--- src/com/cloudant/ejje/ViewServer.java
+++
@@ -1,9 +0,0 @@
-package com.cloudant.ejje;
-
-import com.ericsson.otp.erlang.OtpNode;
-
-public abstract class ViewServer {
-
-	public abstract Runnable getMboxThread(OtpNode node, String name);
-
-}
